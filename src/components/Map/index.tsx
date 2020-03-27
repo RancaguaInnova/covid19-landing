b51e16c3bf3c67c293233e5d4a4de68b2a9c5a19
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-03-27 13:34:19 -0300

    fix text map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,39 +3,44 @@ import ReactGA from 'react-ga'
 import './styles.scss'
 
 const Map: React.FC = () => {
-    return (
-        <div className='map'>
-            <h3 className='text-uppercase'>Mapa de Contingencia</h3>
-            <p className='info-txt'>Información sobre puntos de interés reelevantes para la ciudadanía.</p>
-            <div className='map-box'>
-                <iframe
-                    width='100%'
-                    height='450'
-                    frameBorder='0'
-                    style={{ border: '0' }}
-                    src='https://www.google.com/maps/d/embed?mid=1AHqm7TsKrxWS-qPbevm45XsmBBwRAzOM'
-                    allowFullScreen
-                />
-            </div>
+  return (
+    <div className="map">
+      <h3 className="text-uppercase">Mapa de Contingencia</h3>
+      <p className="info-txt">
+        Información sobre puntos de interés reelevantes para la ciudadanía.
+      </p>
+      <div className="map-box">
+        <iframe
+          width="100%"
+          height="450"
+          frameBorder="0"
+          style={{ border: '0' }}
+          src="https://www.google.com/maps/d/embed?mid=1AHqm7TsKrxWS-qPbevm45XsmBBwRAzOM"
+          allowFullScreen
+        />
+      </div>
 
-            <div className='entrepreneur-box mt-2'>
-                <p className='text-center font-weight-bold'>Si quieres que tu negocio aparezca en este mapa </p>
-                <a
-                    className='btn btn-outline-danger btn-block'
-                    onClick={() => {
-                        ReactGA.event({
-                            category: 'map-form',
-                            action: 'open'
-                        })
-                    }}
-                    href='https://forms.gle/t7xGKcJZx9XfMmDw8'
-                    target='_black'
-                >
-                    INSCRIBETE AQUÍ
-                </a>
-            </div>
-        </div>
-    )
+      <div className="entrepreneur-box mt-2">
+        <p className="text-center font-weight-bold">
+          Si quieres que tu negocio aparece en el siguiente mapa inscríbete en
+          el siguiente formulario:{' '}
+        </p>
+        <a
+          className="btn btn-outline-danger btn-block"
+          onClick={() => {
+            ReactGA.event({
+              category: 'map-form',
+              action: 'open',
+            })
+          }}
+          href="https://forms.gle/t7xGKcJZx9XfMmDw8"
+          target="_black"
+        >
+          INSCRIBETE AQUÍ
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Map
