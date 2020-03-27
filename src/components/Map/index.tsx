94897005e879982d95df267680460601c78442ad
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-03-27 09:58:16 -0300

    mapa actualizado

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,33 +2,37 @@ import React from 'react'
 import './styles.scss'
 
 const Map: React.FC = () => {
-    return (
-        <div className='map'>
-            <h3 className='text-uppercase'>Mapa de Contingencia</h3>
-            <p className='info-txt'>Información sobre puntos de interés reelevantes para la ciudadanía.</p>
-            <div className='map-box'>
-                <iframe
-                    width='100%'
-                    height='450'
-                    frameBorder='0'
-                    style={{ border: '0' }}
-                    src='https://www.google.com/maps/d/u/0/embed?mid=1ESOtdzlMkDyO-6B6JTQUw3t__PmnUgrZ&z=14&ll=-34.1634639999931%2C-70.73531887955562'
-                    allowFullScreen
-                />
-            </div>
+  return (
+    <div className="map">
+      <h3 className="text-uppercase">Mapa de Contingencia</h3>
+      <p className="info-txt">
+        Información sobre puntos de interés reelevantes para la ciudadanía.
+      </p>
+      <div className="map-box">
+        <iframe
+          width="100%"
+          height="450"
+          frameBorder="0"
+          style={{ border: '0' }}
+          src="https://www.google.com/maps/d/embed?mid=1AHqm7TsKrxWS-qPbevm45XsmBBwRAzOM"
+          allowFullScreen
+        />
+      </div>
 
-            <div className='entrepreneur-box mt-2'>
-                <p className='text-center'>Si quieres que tu negocio aparezca en este mapa </p>
-                <a
-                    className='btn btn-outline-danger btn-block'
-                    href='https://forms.gle/t7xGKcJZx9XfMmDw8'
-                    target='_black'
-                >
-                    INSCRIBETE AQUÍ
-                </a>
-            </div>
-        </div>
-    )
+      <div className="entrepreneur-box mt-2">
+        <p className="text-center">
+          Si quieres que tu negocio aparezca en este mapa{' '}
+        </p>
+        <a
+          className="btn btn-outline-danger btn-block"
+          href="https://forms.gle/t7xGKcJZx9XfMmDw8"
+          target="_black"
+        >
+          INSCRIBETE AQUÍ
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Map
