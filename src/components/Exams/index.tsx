14a2e1e9e5f440c3fd35a993c27c87072f2c9ec4
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-03-27 11:57:03 -0300

    add link to exams

--- src/components/Exams/index.tsx
+++ src/components/Exams/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './styles.scss'
 import HealthServicesExams from './healthServicesExams'
-
+import ReactGA from 'react-ga'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 const Exams: React.FC = () => {
   return (
     <div className="exams">
@@ -33,6 +35,86 @@ const Exams: React.FC = () => {
           className="img-fluid"
           alt=""
         />
+      </div>
+      <div>
+        <h5 className="pt-2 pb-2 subtitle"> ¿Qué hacer si tengo covid?</h5>
+        <ul className="list-group pt-4 text-left">
+          <a
+            key={1}
+            onClick={() => {
+              ReactGA.event({
+                category: 'HealthServicesExams',
+                action:
+                  'Indicaciones para personas en aislamiento domiciliario por COVID-19	',
+              })
+            }}
+            href={
+              'https://www.minsal.cl/wp-content/uploads/2020/03/2020.03.13_INDICACIONES-EN-CUARENTENA.pdf'
+            }
+            target="_blank"
+            className="list-group-item list-group-item-action justify-content-center srv-item"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              className="float-right justify-content-center"
+            />
+            <span className="pr-4 w-100">
+              Indicaciones para personas en aislamiento domiciliario por
+              COVID-19
+            </span>
+          </a>
+
+          <a
+            key={1}
+            onClick={() => {
+              ReactGA.event({
+                category: 'HealthServicesExams',
+                action: 'Que hacer si estas con alguien con covid',
+              })
+            }}
+            href={
+              'https://www.nytimes.com/es/2020/03/17/espanol/estilos-de-vida/como-cuidar-mayores-coronavirus.html'
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+            className="list-group-item list-group-item-action justify-content-center srv-item"
+          >
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              className="float-right justify-content-center"
+            />
+            <span className="pr-4 w-100">
+              {' '}
+              Que hacer si estas con alguien con covid{' '}
+            </span>
+          </a>
+          <a
+            key={1}
+            onClick={() => {
+              ReactGA.event({
+                category: 'HealthServicesExams',
+                action:
+                  'Protocolo de limpieza y desinfección de ambientes COVID-19',
+              })
+            }}
+            href={
+              'https://www.minsal.cl/wp-content/uploads/2020/03/PROTOCOLO-DE-LIMPIEZA-Y-DESINFECCI%C3%93N-DE-AMBIENTES-COVID-19.pdf'
+            }
+            target="_blank"
+            rel="noopener noreferrer"
+            className="list-group-item list-group-item-action justify-content-center srv-item"
+          >
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              className="float-right justify-content-center"
+            />
+            <span className="pr-4 w-100">
+              {' '}
+              Protocolo de limpieza y desinfección de ambientes COVID-19{' '}
+            </span>
+          </a>
+        </ul>
       </div>
     </div>
   )
