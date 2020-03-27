a4c4b684c05013bd92285c86ae0d8a39e89c239e
José Abarca Saavedra <jose@smart.rancagua.cl>
2020-03-27 11:54:19 -0300

    exams adjustment

--- src/components/Exams/index.tsx
+++ src/components/Exams/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './styles.scss'
+import ReactGA from 'react-ga'
 import HealthServicesExams from './healthServicesExams'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 const Exams: React.FC = () => {
     return (
         <div className='exams'>
@@ -17,7 +19,7 @@ const Exams: React.FC = () => {
 
             <div>
                 <div>
-                    <h4 className='pt-2 pb-2 subtitle'>¿Qué hacer con mi examen?</h4>
+                    <h4 className='pt-2 pb-2 subtitle'>¿Qué debo hacer con mi examen?</h4>
                 </div>{' '}
                 <img src='/assets/images/seccion_coronavirus-02.jpg' className='img-fluid' alt='' />
             </div>
@@ -26,6 +28,36 @@ const Exams: React.FC = () => {
                     <h4 className='pt-2 pb-2 subtitle'>¿Qué hacer si mi resultado es positivo?</h4>
                 </div>{' '}
                 <img src='/assets/images/seccion_coronavirus-02.jpg' className='img-fluid' alt='' />
+                <ul className='list-group pt-4 text-left'>
+                    <a
+                        onClick={() => {
+                            ReactGA.event({
+                                category: 'covid-document',
+                                action: ''
+                            })
+                        }}
+                        href=''
+                        target='_blank'
+                        className='list-group-item list-group-item-action justify-content-center srv-item'
+                    >
+                        <FontAwesomeIcon icon={faChevronRight} className='float-right justify-content-center' />
+                        <span className='pr-4 w-100'>Link a PDF 1</span>
+                    </a>
+                    <a
+                        onClick={() => {
+                            ReactGA.event({
+                                category: 'covid-document',
+                                action: ''
+                            })
+                        }}
+                        href=''
+                        target='_blank'
+                        className='list-group-item list-group-item-action justify-content-center srv-item'
+                    >
+                        <FontAwesomeIcon icon={faChevronRight} className='float-right justify-content-center' />
+                        <span className='pr-4 w-100'>Link a PDF 2</span>
+                    </a>
+                </ul>
             </div>
         </div>
     )
