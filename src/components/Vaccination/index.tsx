8ba77c4e09a61622ea80f8a8989d93c3a7aeb6d3
jeperezr <jaime.e.perez.r@hotmail.com>
2020-04-22 17:49:20 -0400

    update covid-19

--- src/components/Vaccination/index.tsx
+++ src/components/Vaccination/index.tsx
@@ -1,48 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import app from 'providers/firebase'
-import { isMobile } from 'mobile-device-detect'
+import React, { useEffect, useState } from "react";
+import app from "providers/firebase";
+import { isMobile } from "mobile-device-detect";
 
-import Accordion from 'react-bootstrap/Accordion'
-import { collectionData } from 'rxfire/firestore'
-import _isEqual from 'lodash/isEqual'
-import _sortBy from 'lodash/sortBy'
-import Loading from 'components/Loading'
-import VaccinationItem from './VaccinationItem'
-import './styles.scss'
+import Accordion from "react-bootstrap/Accordion";
+import { collectionData } from "rxfire/firestore";
+import _isEqual from "lodash/isEqual";
+import _sortBy from "lodash/sortBy";
+import Loading from "components/Loading";
+import VaccinationItem from "./VaccinationItem";
+import "./styles.scss";
 
 const Vaccination: React.FC = () => {
-    const [ services, setServices ] = useState<any[]>([])
-    const [ loading, setLoading ] = useState<boolean>(true)
-    const [ current, setCurrent ] = useState<any>(null)
-    useEffect(() => {
-        var collectionRef = null
-        async function loadServices() {
-            const servicesRef = app.firestore().collection('vaccinations')
-            collectionRef = collectionData(servicesRef, 'id')
+  const [services, setServices] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [current, setCurrent] = useState<any>(null);
+  useEffect(() => {
+    var collectionRef = null;
+    async function loadServices() {
+      const servicesRef = app.firestore().collection("vaccinations");
+      collectionRef = collectionData(servicesRef, "id");
 
-            collectionRef.subscribe((list: any = []) => {
-                // Prevent innecesary updates
-                if (!_isEqual(list, services)) {
-                    setServices(list)
-                    setLoading(false)
-                }
-            })
+      collectionRef.subscribe((list: any = []) => {
+        // Prevent innecesary updates
+        if (!_isEqual(list, services)) {
+          setServices(list);
+          setLoading(false);
         }
-
-        loadServices()
-    })
-
-    const onSelectItem = (item: any) => {
-        const same = _isEqual(item, current)
-        setCurrent(same ? null : item)
+      });
     }
-    return (
-        <div className={`vaccination ${isMobile ? '' : 'row'}`}>
-            <div className={isMobile ? '' : 'offset-1 col-5'}>
-                <h3 className='pb-2 text-uppercase'>Campaña Influenza 2020</h3>
-                <h5 className='subtitle'>¿Quiénes deben vacunarse contra la influenza?</h5>
 
-                <div className='container-fluid'>
+    loadServices();
+  });
+
+  const onSelectItem = (item: any) => {
+    const same = _isEqual(item, current);
+    setCurrent(same ? null : item);
+  };
+  return (
+    <div className={`vaccination ${isMobile ? "" : "row"}`}>
+      <div className={isMobile ? "" : "offset-1 col-5"}>
+        <h3 className="pb-2 text-uppercase">Campaña Influenza 2020</h3>
+        {/*                 <h5 className='subtitle'>¿Quiénes deben vacunarse contra la influenza?</h5>
+         */}
+        {/*       <div className='container-fluid'>
                     <div className='row'>
                         <div className='col-md-12'>
                             <ul className='lista'>
@@ -83,32 +83,41 @@ const Vaccination: React.FC = () => {
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className={isMobile ? '' : 'col-5'}>
-                <h5 className='pt-2 pb-2 subtitle'>¿Dónde vacunarse?</h5>
+                </div> */}
+        <img
+          src="assets/images/influenza.jpeg"
+          alt="influeza"
+          width="100%"
+          className="img-fluid"
+        ></img>
+      </div>
+      <div className={isMobile ? "" : "col-5"}>
+        <h5 className="pt-2 pb-2 subtitle">¿Dónde vacunarse?</h5>
 
-                {loading && <Loading />}
+        {loading && <Loading />}
 
-                {!loading && (
-                    <div className='pl-2 pr-2 vaccination-box'>
-                        <Accordion>
-                            {_sortBy(services.filter((s) => s.active), (s) => {
-                                return s.name_place
-                            }).map((service, idx) => (
-                                <VaccinationItem
-                                    service={{ ...service, idx }}
-                                    current={current}
-                                    onSelect={onSelectItem}
-                                    key={idx}
-                                />
-                            ))}
-                        </Accordion>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
-}
+        {!loading && (
+          <div className="pl-2 pr-2 vaccination-box">
+            <Accordion>
+              {_sortBy(
+                services.filter((s) => s.active),
+                (s) => {
+                  return s.name_place;
+                }
+              ).map((service, idx) => (
+                <VaccinationItem
+                  service={{ ...service, idx }}
+                  current={current}
+                  onSelect={onSelectItem}
+                  key={idx}
+                />
+              ))}
+            </Accordion>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Vaccination
+export default Vaccination;
