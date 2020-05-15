1b687e5077c99c8f23df0130ae86e0f50a2fe26c
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-05-15 17:01:34 -0400

    update cifras 15 de mayo

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -6,14 +6,14 @@ const Statistic: React.FC = () => {
         <div>
             <h3 className='text-uppercase'>CIFRAS DE CONTAGIO</h3>
             {/*  <h5 className='hash'>#fase4rancagua</h5> */}
-             <p className='last-update'>Última actualización 14 Mayo 2020 14:40hrs</p>
+             <p className='last-update'>Última actualización 15 Mayo 2020 17:00hrs</p>
             <hr />
             <div className='row'>
                 <div className='col-md-6 col-xs-12'>
-                    <img className='img-fluid' src='/assets/images/covid-map.jpg' />
+                    <img className='img-fluid' src='/assets/images/covid-map.jpeg' />
                 </div>
                 <div className='col-md-6 col-xs-12'>
-                    <img className='img-fluid mt-2' src='/assets/images/covid-graph.jpg' />
+                    <img className='img-fluid mt-2' src='/assets/images/covid-graph.jpeg' />
                 </div>
             </div>
            
