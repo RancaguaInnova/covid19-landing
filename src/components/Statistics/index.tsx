9d58719e2c4d162df399e7daf08fba9504872007
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-05-28 12:39:29 -0400

    add video landing covid

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -5,6 +5,7 @@ import { collectionData } from "rxfire/firestore";
 import _isEqual from "lodash/isEqual";
 import _sortBy from "lodash/sortBy";
 import Loading from "components/Loading";
+import ReactPlayer from 'react-player'
 
 const Statistic: React.FC = () => {
     const [statictics, setStatictics] = useState<any[]>([]);
@@ -30,20 +31,48 @@ const Statistic: React.FC = () => {
             {/*  <h5 className='hash'>#fase4rancagua</h5> */}
             {loading && <Loading />}
             {!loading && (
-                statictics.map((item,index) =>
-                <div key={index}>
-                    <p className='last-update'>Última actualización {new Date( item.date.seconds *1000).toLocaleString()} hrs</p>
-                    <hr />
-                    <div className='row'>
-                        <div className='col-md-6 col-xs-12'>
-                            <img className='img-fluid' src={item.covidMap.src} />
+                statictics.map((item, index) =>
+                    <div key={index}>
+                        <p className='last-update'>Última actualización {new Date(item.date.seconds * 1000).toLocaleString()} hrs</p>
+                        <hr />
+                        {item.urlVideo && item.urlVideo != '' ? (
+                            <div className='row'>
+                                <div className='col-md-12 col-xs-12'>
+                                <div   className='player-wrapper'>
+                                    <ReactPlayer
+                                              className='react-player'
+
+                                        url={item.urlVideo}
+                                        config={{
+                                            youtube: {
+                                                playerVars: { showinfo: 1 }
+                                            },
+                                        }}
+                                        width='100%'
+                                        height='100%'
+                                    />
+                                    </div>
+                                </div>
+                            </div>
+                        ) : null}
+
+
+
+                        <div className='row'>
+                            {item.covidMap && item.covidMap.src && item.covidMap.src != '' ? (
+                                <div className='col-md-6 col-xs-12'>
+                                    <img className='img-fluid' src={item.covidMap.src} />
+                                </div>
+                            ) : null}
+                            {item.covidGraph && item.covidGraph.src && item.covidGraph.src != '' ? (
+                                <div className='col-md-6 col-xs-12'>
+                                    <img className='img-fluid mt-2' src={item.covidGraph.src} />
+                                </div>
+                            ) : null}
                         </div>
-                        <div className='col-md-6 col-xs-12'>
-                            <img className='img-fluid mt-2'  src={item.covidGraph.src} />
-                        </div>
+
                     </div>
-                </div>
-           ))}
+                ))}
         </div>
     )
 }
